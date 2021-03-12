b211864194f9d1a079a601a41398c127260d8d69
mohosin16103308 <hamza1610330816@gmail.com>
2021-03-12 16:55:13 +0600

    08.Typing Mon Star Game Design - Text Field Editable and Not Editable

--- src/typing_monstar_game/Home.js
+++ src/typing_monstar_game/Home.js
@@ -1,25 +1,41 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 
 import './Home.css'
 import toss from "./toss.gif"
 
-function Home () {
-    return(
-        <div className="main-area">
-     
-     <h1 className="heading-h1">Let's Play Typing Speed Game</h1>
-       <h2>Remaining Time = <i class="fas fa-clock"></i>  60 s </h2>
-     <img src={ toss } style={{ width:"250px",height:"100px", paddingBottom:"20px" }} />
+function Home() {
 
-       <div className="textAreaStyle">
-       <textarea cols="100" rows="20"/>
-       </div>
-           
-           <button className="button-start">Start</button>
+  // Variable that store Edit Able or not Edit Able css 
+  let non_edit
 
-        </div>
-    )
+  const [nonEdit, setnonEdit] = useState(true)
+
+  if (nonEdit) {
+
+    non_edit = { pointerEvents: "none", backgroundColor: "green", color: "#fff" }
+
+  } else {
+
+    non_edit = { backgroundColor: "#fff", color: "#000" }
+    
+  }
+
+  return (
+    <div className="main-area">
+
+      <h1 className="heading-h1">Let's Play Typing Speed Game</h1>
+      <h2>Remaining Time =  60 s </h2>
+      <img src={toss} style={{ width: "250px", height: "100px", paddingBottom: "20px" }} />
+
+      <div className="textAreaStyle">
+        <textarea cols="100" rows="20" style={non_edit} />
+      </div>
+
+      <button className="button-start">Start</button>
+
+    </div>
+  )
 }
 
 
