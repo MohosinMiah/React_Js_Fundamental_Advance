70b2b0dd33fc5cb19e82ebed43f64b798f7b98a9
mohosin16103308 <hamza1610330816@gmail.com>
2021-03-22 10:56:55 +0600

    Typing Monstar Game Done Verson 1

--- src/typing_monstar_game/Home.js
+++ src/typing_monstar_game/Home.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect, useRef } from 'react';
 
 
 import './Home.css'
@@ -9,7 +9,17 @@ function Home() {
   // Variable that store Edit Able or not Edit Able css 
   let non_edit
 
+  const [wordLength,setwordLength] = useState(0)
+
   const [nonEdit, setnonEdit] = useState(true)
+
+  const [counter, setCounter] = useState('Pending');
+
+  const [wordCount,setwordCount] = useState("")
+  
+  // Store a reference to the input's DOM node
+  const inputRef = useRef();
+
 
   if (nonEdit) {
 
@@ -18,21 +28,76 @@ function Home() {
   } else {
 
     non_edit = { backgroundColor: "#fff", color: "#000" }
-    
+
   }
+
+
+  useEffect(
+    () => {
+      // This runs AFTER the first render,
+      // so the ref is set by now.
+      inputRef.current.focus();
+    },
+		// The effect "depends on" inputRef
+    [inputRef]
+  );
+
+
+  useEffect(() => {
+
+
+    if (counter > 0) {
+      console.log("Start Game")
+
+      counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+
+    }
+
+    if (counter == 0) {
+      setnonEdit((!nonEdit))
+      console.log("NON EDIT CHANGED")
+        setwordLength(wordCount.split(' ').length)
+
+       console.log(wordLength)
+
+    }
+
+   
+
+  }, [counter]);
+
+
+  function startGameHandler() {
+
+    setwordCount("")
+    setnonEdit((!nonEdit))
+    setCounter(5)
+
+  }
+
 
   return (
     <div className="main-area">
 
       <h1 className="heading-h1">Let's Play Typing Speed Game</h1>
-      <h2>Remaining Time =  60 s </h2>
+      <h2>Remaining Time =  {counter} s </h2>
+
       <img src={toss} style={{ width: "250px", height: "100px", paddingBottom: "20px" }} />
 
       <div className="textAreaStyle">
-        <textarea cols="100" rows="20" style={non_edit} />
+        <textarea cols="100" rows="20" style={non_edit}   
+
+        ref={inputRef}
+        value={wordCount}
+        onChange={e => setwordCount(e.target.value)} 
+        
+        />
+
+        {nonEdit == true  ? <p className="typedResult">Your Typied {wordLength } Words in a Minute (60 s).Start Game by clicking <strong className="heading-h1">Start Button</strong></p> : ""}
+
       </div>
 
-      <button className="button-start">Start</button>
+      <button className="button-start" onClick={startGameHandler}>Start  </button>
 
     </div>
   )
