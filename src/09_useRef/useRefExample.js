430f7aa2d922e99657cd5cf4d463e064cbd3c73b
mohosin16103308 <hamza1610330816@gmail.com>
2021-03-28 10:28:02 +0600

    UseRef Added to Do List

--- src/09_useRef/useRefExample.js
+++ src/09_useRef/useRefExample.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react"
+import React, {useState,useRef} from "react"
 
 function useRefExample() {
     const [newTodoValue, setNewTodoValue] = useState("")
     const [todosList, setTodosList] = useState([])
+    const inputRef = useRef(null)
 
     // Set New Value
     function handleChange(event) {
@@ -15,6 +16,10 @@ function useRefExample() {
         event.preventDefault()
         setTodosList(prevTodosList => [...prevTodosList, newTodoValue])
         setNewTodoValue("")
+
+        // Get Back Cursor  to the Input Field or Focus on Input Field
+        inputRef.current.focus()
+        
     }
     
     const allTodos = todosList.map(todo => <p key={todo}>{todo}</p>)
@@ -22,7 +27,7 @@ function useRefExample() {
     return (
         <div>
             <form>
-                <input type="text" name="todo" value={newTodoValue} onChange={handleChange}/>
+                <input ref={inputRef} type="text" name="todo" value={newTodoValue} onChange={handleChange}/>
                 <button onClick={addTodo}>Add todo item</button>
             </form>
             {allTodos}
